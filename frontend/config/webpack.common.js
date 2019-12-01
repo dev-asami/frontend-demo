f65dde35affbd9579b667b62f16a1300dca088f6
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2019-12-02 06:02:48 +0900

    フロントエンド系のファイルをすべてfrontendディレクトに移動
    ファイルが散らばって見通しが悪くなって来たため。

--- frontend/config/webpack.common.js
+++ frontend/config/webpack.common.js
@@ -1,10 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/main/frontend/credit-tp.js',
+  entry: path.resolve(__dirname, '../src/credit-tp.js'),
   output: {
     filename: 'credit-tp.js',
-    path: path.join(__dirname, 'target/classes/static/js')
+    path: path.resolve(__dirname, '../../target/classes/static/js')
   },
   module: {
     rules: [
